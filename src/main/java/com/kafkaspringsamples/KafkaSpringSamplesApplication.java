29e637c7cd7bb8e43265b8ef80e7447446a1a92e
godwinmg <godwingeorge@pop-os.localdomain>
2024-01-15 22:04:22 -0500

    adding the project structure

---
+++ src/main/java/com/kafkaspringsamples/KafkaSpringSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaspringsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringSamplesApplication.class, args);
+	}
+
+}
